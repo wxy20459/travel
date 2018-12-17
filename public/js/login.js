93d0b49fcb0226a7b02a1cc815afc93b0a7bf5b3
wxy20459 <17738008895@163.com>
2018-12-17 21:06:55 +0800

    修改首页静态页面！

---
+++ public/js/login.js
@@ -0,0 +1,11 @@
+$(function(){
+    $("<link rel='stylesheet' href='css/login.css'>").appendTo("head");
+    $.ajax({
+      url:"http://127.0.0.1:3000/header.html",
+      type:"get",
+      success:function(res){
+        //res->html片段
+        $(res).replaceAll("#login");
+      }
+    })
+})
