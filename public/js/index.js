864dd7a8f385f5664710f0570e4b6e25cd5b244f
wxy20459 <17738008895@163.com>
2019-01-06 18:44:40 +0800

    页面效果添加及代码优化

--- public/js/index.js
+++ public/js/index.js
@@ -115,7 +115,7 @@ $(function () {
                 $('.list li').eq(nowIndex).addClass('active');
             }
 
-            //第二个小功能: -- loading --
+            //第二个小功能: -- 轮播、loading --
             var nowTop = 0;
             var decretion = 1;
             var loadTimer = setInterval(() => {
@@ -143,109 +143,101 @@ $(function () {
                 start();
             })
 
-            //第三个小功能： -- 图片放大和过渡效果 --
-            // one
-            $('.travel>.more>.container>.row>.one>div>img')
-                // 移入
-                .on('mouseenter', function () {
-                    //小火柴过渡效果
-                    $('.travel>.more>.container>.row>.one>div>p').css({ 'width': '92%', 'transition': '1s' });
-                    //card阴影
-                    $('.travel>.more>.container>.row>.one').css({ 'box-shadow': '0px 16px 10px -15px #999' });
-                })
-                // 移出
-                .on('mouseleave', function () {
-                    //移除小火柴
-                    $('.travel>.more>.container>.row>.one>div>p').css({ 'width': 0, 'transition': '1s' });
-                    //移除阴影
-                    $('.travel>.more>.container>.row>.one').css({ "box-shadow": 'none' });
-                })
-            // two
-            $('.travel>.more>.container>.row>.two>div>img')
-                // 移入
-                .on('mouseenter', function () {
-                    //小火柴过渡效果
-                    $('.travel>.more>.container>.row>.two>div>p').css({ 'width': '92%', 'transition': '1s' });
-                    //card阴影
-                    $('.travel>.more>.container>.row>.two').css({ 'box-shadow': '0px 16px 10px -15px #999' });
-                })
-                // 移出
-                .on('mouseleave', function () {
-                    //移除小火柴
-                    $('.travel>.more>.container>.row>.two>div>p').css({ 'width': 0, 'transition': '1s' });
-                    //移除阴影
-                    $('.travel>.more>.container>.row>.two').css({ "box-shadow": 'none' });
-                })
-            // three
-            $('.travel>.more>.container>.row>.three>div>img')
-                // 移入
-                .on('mouseenter', function () {
-                    //小火柴过渡效果
-                    $('.travel>.more>.container>.row>.three>div>p').css({ 'width': '92%', 'transition': '1s' });
-                    //card阴影
-                    $('.travel>.more>.container>.row>.three').css({ 'box-shadow': '0px 16px 10px -15px #999' });
-                })
-                // 移出
-                .on('mouseleave', function () {
-                    //移除小火柴
-                    $('.travel>.more>.container>.row>.three>div>p').css({ 'width': 0, 'transition': '1s' });
-                    //移除阴影
-                    $('.travel>.more>.container>.row>.three').css({ "box-shadow": 'none' });
-                })
-            // 第四个小功能 --字体加背景--
-            // one
-            $('.target_body>.container>.row>.one>div>img')
-                // 移入
-                .on('mouseenter', function () {
-                    $('.target_body>.container>.row>.one>h4').css({ 'background-color': '#0fc7d1' });
-                    $('.target_body>.container>.row>.one>h4>p,.target_body>.container>.row>.one>h4>a').css({ 'color': '#fff' });
-                })
-                // 移出
-                .on('mouseleave', function () {
-                    $('.target_body>.container>.row>.one>h4').css({ 'background-color': '', 'transition': '1s' });
-                    $('.target_body>.container>.row>.one>h4>a').css({ 'color': 'rgb(82, 79, 79)' });
-                    $('.target_body>.container>.row>.one>h4>p').css({ 'color': '#999' })
-                })
 
-            // two
-            $('.target_body>.container>.row>.two>div>img')
-                // 移入
-                .on('mouseenter', function () {
-                    $('.target_body>.container>.row>.two>h4').css({ 'background-color': '#0fc7d1' });
-                    $('.target_body>.container>.row>.two>h4>p,.target_body>.container>.row>.two>h4>a').css({ 'color': '#fff' });
+            //第三个小功能： -- 轮播下、图片放大和过渡效果 --
+            $(".travel>.more>.container>.row>div>div:first-child>img")
+                .on("mouseenter",function(){
+                    //小火柴过渡效果
+                    $(this).parent("div").siblings("div").children("p").css({'width':'92%','transition':'1s'});
+                    //card阴影
+                    $(this).parent("div").parent("div").css({'box-shadow':'0px 16px 10px -15px #999'})
+                })
+                .on('mouseleave', function() {
+                    //移除小火柴
+                    $(this).parent("div").siblings("div").children("p").css({ 'width': 0, 'transition': '1s' });
+                    //移除阴影
+                    $(this).parent("div").parent("div").css({ "box-shadow": 'none'})
+                })
+            
+            // 第四个小功能 --目的地、字体加背景--
+            $(".target_body>.container>.row>div>div>img")
+                .on('mouseenter',function(){
+                    $(this).parent("div").next().css({ 'background-color': '#0fc7d1' })
+                    $(this).parent("div").next().children().css({ 'color': '#fff' })
                 })
                 // 移出
                 .on('mouseleave', function () {
-                    $('.target_body>.container>.row>.two>h4').css({ 'background-color': '', 'transition': '1s' });
-                    $('.target_body>.container>.row>.two>h4>a').css({ 'color': 'rgb(82, 79, 79)' });
-                    $('.target_body>.container>.row>.two>h4>p').css({ 'color': '#999' })
+                    $(this).parent("div").next().css({ 'background-color': '', 'transition': '1s' });
+                    $(this).parent("div").next().children("a").css({ 'color': 'rgb(82, 79, 79)' });
+                    $(this).parent("div").next().children("p").css({ 'color': '#999'})
                 })
-            // three
-            $('.target_body>.container>.row>.three>div>img')
-                // 移入
-                .on('mouseenter', function () {
-                    $('.target_body>.container>.row>.three>h4').css({ 'background-color': '#0fc7d1' });
-                    $('.target_body>.container>.row>.three>h4>p,.target_body>.container>.row>.three>h4>a').css({ 'color': '#fff' });
-                })
-                // 移出
-                .on('mouseleave', function () {
-                    $('.target_body>.container>.row>.three>h4').css({ 'background-color': '', 'transition': '1s' });
-                    $('.target_body>.container>.row>.three>h4>a').css({ 'color': 'rgb(82, 79, 79)' });
-                    $('.target_body>.container>.row>.three>h4>p').css({ 'color': '#999' })
-                })
-            // four
-            $('.target_body>.container>.row>.four>div>img')
-                // 移入
-                .on('mouseenter', function () {
-                    $('.target_body>.container>.row>.four>h4').css({ 'background-color': '#0fc7d1' });
-                    $('.target_body>.container>.row>.four>h4>p,.target_body>.container>.row>.four>h4>a').css({ 'color': '#fff' });
-                })
-                // 移出
-                .on('mouseleave', function () {
-                    $('.target_body>.container>.row>.four>h4').css({ 'background-color': '', 'transition': '1s' });
-                    $('.target_body>.container>.row>.four>h4>a').css({ 'color': 'rgb(82, 79, 79)' });
-                    $('.target_body>.container>.row>.four>h4>p').css({ 'color': '#999' })
-                })
+            // 第五个小功能 -- 热门路线、日期变色 --
+            $(".travel>.hot>.row>.contain")
+            .on("mouseenter",function(){
+                // $(this).children(".date").css({'color':'#0fc7d1'});//方法1
+                $(this).children().first().css({'color':'#0fc7d1','font-weight':'400'});//方法2
+            })
+            .on('mouseleave',function(){
+                $(this).children().first().css({'color':'#999','font-weight':'400'});//方法2
+            })
+            // 第六个小功能 -- 特色酒店card效果 --
+            $(".hotel>.container>.box>.contain")
+            // 移入
+            .on('mouseenter',function(){
+                $(this).css({'box-shadow':'1px 4px 10px #999'});
+                $(this).children().children('a').css({'color':'#0fc7d1'});
+                $(this).children('a').css({'right':'20px','border':'1px solid #0fc7d1','color':'#0fc7d1'});
+            })
+            // 移出
+            .on('mouseleave',function(){
+                $(this).css({'box-shadow':'none'})
+                $(this).children().children('a').css({'color':'#333'});
+                $(this).children('a').css({'right':'35px','border':'1px solid #ddd','color':'#ddd'});
+            })
+
+/* ----------------------------------------------------------------------------------------------------------------------------- */
+            //保留两位小数并且整数部分三位一个逗号分隔符的数字金钱标准表示法：
+            //这里假设我们即不知道输入数字的整数位数，也不知道小数位数
+            /*将100000转为100,000.00形式*/
+            var dealNumber = function(money){
+                if(money && money!=null){
+                    money = String(money);
+                    var left=money.split('.')[0],right=money.split('.')[1];
+                    right = right ? (right.length>=2 ? '.'+right.substr(0,2) : '.'+right+'0') : '.00';
+                    var temp = left.split('').reverse().join('').match(/(\d{1,3})/g);
+                    return (Number(money)<0?"-":"") + temp.join(',').split('').reverse().join('')+right;
+                }else if(money===0){   //注意===在这里的使用，如果传入的money为0,if中会将其判定为boolean类型，故而要另外做===判断
+                    return '0.00';
+                }else{
+                    return "";
+                }
+            };
+            /*将100,000.00转为100000形式*/
+            var undoNubmer = function(money){
+                if(money && money!=null){
+                    money = String(money);
+                    var group = money.split('.');
+                    var left = group[0].split(',').join('');
+                    return Number(left+"."+group[1]);
+                }else{
+                    return "";
+                }
+            };
+/* ----------------------------------------------------------------------------------------------------------------------------- */
+
+            //第六个小功能  -- 小海豚、数据 --
+            $('.travel>.dolphin>.container>.row>.data_1>h5').on('mouseenter',function(){
+                console.log($(this).html()); 
+                // console.log(parseInt($(this).html()));// 10
+                // 拿网页上的数据、parseInt一下.toFixed(2)
+                console.log(undoNubmer('10,000.00'));//10000
+                // 
+                console.log(dealNumber(10000));//10,000.00
+            })
+
+            
+
+
         }
     })
 })
